3d3ce183d74e6a8f4e0b0dd75928397ff66f2f24
RocketWeb99 <97522953+RocketWeb99@users.noreply.github.com>
2022-02-23 12:22:06 +0000

    Delete src directory

--- src/Components/Input/Input.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Input.module.scss";
-
-const Input = () => {
-  return <input className={styles.input} placeholder="Type something" />;
-};
-
-export default Input;
